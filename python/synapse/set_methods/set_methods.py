d8cee354ac89423465fcea4ac657c8b1a2450021
Leonard Zhong <leonard.c.zhong@gmail.com>
2023-03-28 20:07:01 +1100

    Update set_methods.py
    added copy method example

--- python/synapse/set_methods/set_methods.py
+++ python/synapse/set_methods/set_methods.py
@@ -1,8 +1,14 @@
 first_set = set([1, 2, 4, 5, 10, 19])
 second_set = set([1, 2, 3, 6, 7, 8, 19])
 
+# copy method
+third_set = first_set.copy()
+print(third_set)
+
 # intersection = first_set & second_set
 def get_intersection(set1, set2):
     return set1.intersection(set2)
 
 print(get_intersection(first_set, second_set))
+
+
