5613cc4dbfa43b4b6d76b452012ab672b3eb23a3
Leonard Zhong <leonard.c.zhong@gmail.com>
2023-03-28 19:39:38 +1100

    Update set_methods.py
    def print intersection function

--- python/synapse/set_methods/set_methods.py
+++ python/synapse/set_methods/set_methods.py
@@ -2,5 +2,7 @@ first_set = set([1, 2, 4, 5, 10, 19])
 second_set = set([1, 2, 3, 6, 7, 8, 19])
 
 # intersection = first_set & second_set
-intersection = first_set.intersection(second_set)
-print(intersection)
+def using_intersection(set1, set2):
+    print(set1.intersection(set2))
+
+using_intersection(first_set, second_set)
