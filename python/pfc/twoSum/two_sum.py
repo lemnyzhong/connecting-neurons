f4c59a1538e5fceadb0a43ad9e7a07c9bd87197f
Leonard Zhong <leonard.c.zhong@gmail.com>
2023-03-29 10:36:09 +1100

    Update two_sum.py

--- python/pfc/twoSum/two_sum.py
+++ python/pfc/twoSum/two_sum.py
@@ -1,22 +1,10 @@
-# THIS IS TWO SUM
-# Find a pair with the given sum in an array
-# Given an unsorted integer array, find a pair with the given sum in it.
-# Example: nums = [8, 7, 2, 5, 3, 1]
-#          target = 10
- 
-#          Output:
- 
-#          Pair found (8, 2)
-#          or
-#          Pair found (7, 3)
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-#          or None case:
- 
-#          nums = [5, 2, 6, 8, 1, 9]
-#          target = 12
- 
-#          Output: Pair not found
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
+# You can return the answer in any order.
 
-def two_sum(arr):
-    return 0
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
