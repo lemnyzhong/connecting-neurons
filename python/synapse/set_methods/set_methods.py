2b99428d1314830adde8912fc58005d57a6ee3cb
Leonard Zhong <leonard.c.zhong@gmail.com>
2023-03-28 20:21:53 +1100

    Update set_methods.py
    added difference() method example

--- python/synapse/set_methods/set_methods.py
+++ python/synapse/set_methods/set_methods.py
@@ -5,10 +5,23 @@ second_set = set([1, 2, 3, 6, 7, 8, 19])
 third_set = first_set.copy()
 print(third_set)
 
-# intersection = first_set & second_set
+
+# returns the common element values in 2 sets
+# uses & or .intersecton()
 def get_intersection(set1, set2):
-    return set1.intersection(set2)
+    return set1 & set2
+    # return set1.intersection(set2)
 
 print(get_intersection(first_set, second_set))
+
+
+# returns the non common element values in 2 sets
+# uses '-' or .difference()
+def get_difference(set1, set2):
+    return set1 - set2
+    # return set1.difference(set2)
+
+print(get_difference(first_set, second_set))
+print(get_difference(second_set, first_set))
 
 
