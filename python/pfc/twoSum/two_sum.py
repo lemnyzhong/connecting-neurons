27e6e2a33050b5e6e29b69b1ab77e59544b2fd0c
Leonard Zhong <leonard.c.zhong@gmail.com>
2023-03-29 09:43:21 +1100

    created two_sum

---
+++ python/pfc/twoSum/two_sum.py
@@ -0,0 +1,22 @@
+# THIS IS TWO SUM
+# Find a pair with the given sum in an array
+# Given an unsorted integer array, find a pair with the given sum in it.
+# Example: nums = [8, 7, 2, 5, 3, 1]
+#          target = 10
+ 
+#          Output:
+ 
+#          Pair found (8, 2)
+#          or
+#          Pair found (7, 3)
+
+#          or None case:
+ 
+#          nums = [5, 2, 6, 8, 1, 9]
+#          target = 12
+ 
+#          Output: Pair not found
+
+
+def two_sum(arr):
+    return 0
