4f1bffbb7547d73a124ce1e6cca7d52d6cf7854e
Leonard Zhong <leonard.c.zhong@gmail.com>
2023-03-28 19:35:51 +1100

    Delete set_methods.py

--- python/synapse/set_intersection/set_methods.py
+++
@@ -1,6 +0,0 @@
-first_set = set([1, 2, 4, 5, 10, 19])
-second_set = set([1, 2, 3, 6, 7, 8, 19])
-
-# intersection = first_set & second_set
-intersection = first_set.intersection(second_set)
-print(intersection)
