177e161b724cb8a12663f44954674c3961072578
Leonard Zhong <leonard.c.zhong@gmail.com>
2023-03-30 18:40:38 +1100

    clean up

--- python/prefrontal cortex/two_sum/two_sum.py
+++ python/prefrontal cortex/two_sum/two_sum.py
@@ -1,13 +1,14 @@
-# Two Sum (leetcode #1)
-# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-# You may assume that each input would have exactly one solution, and you may not use the same element twice.
-# You can return the answer in any order.
+'''
+Two Sum (leetcode #1)
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
 
-# Example
-# Input: nums = [2,7,11,15], target = 9
-# Output: [0,1]
-# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-
+Example
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+'''
 # global arr
 arr = [2, 1, 5, 3]
 
