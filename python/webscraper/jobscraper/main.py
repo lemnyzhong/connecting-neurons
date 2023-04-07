f81e0adb081665752a4717163a616947393be44b
Leonard Zhong <leonard.c.zhong@gmail.com>
2023-04-07 23:10:46 +1000

    cleaning up jobscraper

--- python/webscraper/jobscraper/main.py
+++ python/webscraper/jobscraper/main.py
@@ -2,26 +2,31 @@ from bs4 import BeautifulSoup
 import requests
 import webbrowser
 
-rq = requests.get('https://www.seek.com.au/python-jobs/in-All-Sydney-NSW').text
+# extracting web data and booting it into BeautifulSoup
+rq = requests.get('https://www.seek.com.au/programming-jobs/in-All-Sydney-NSW').text
 soup = BeautifulSoup(rq, 'lxml')
 
+# searching specific html elements for roles titles
+# and employer's names
 job_div = soup.find_all('h3', class_='_1wkzzau0 a1msqi4y lnocuo0 lnocuol _1d0g9qk4 lnocuos lnocuo21')
 employers_span = soup.find_all('span', class_='_1wkzzau0 a1msqi4y lnocuo0 lnocuo2 lnocuo21 _1d0g9qk4 lnocuod')
 
-# prints job titles
+# initialise arrays for storing collected data
 current_roles = []
-for i in job_div:
-    current_roles.append(i.text)
-
 employers = []
+
+# append data to storage
 for i in range(0, len(employers_span)):
+    current_roles.append(job_div[i].text)
     employers.append(employers_span[i].text)
 
+# print stored job roles with respective employers
 for i in range(len(current_roles)):
     print(f'{current_roles[i]} {employers[i]}\n')
 
+# if you see anything interesting bust open the webpage
 if(input("open?\n").lower() == 'yes'):
-    webbrowser.open_new('https://www.seek.com.au/python-jobs/in-All-Sydney-NSW')
+    webbrowser.open_new('https://www.seek.com.au/programming-jobs/in-All-Sydney-NSW')
 
 
 
