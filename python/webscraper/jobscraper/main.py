5d9c3ea9947c6b4c1f7e5be7d4a1669c9c7fb195
Leonard Zhong <leonard.c.zhong@gmail.com>
2023-04-07 22:59:49 +1000

    jobscraper
    webscrapes seek.com for latest python related jobs
    posts employer as well

--- python/webscraper/jobscraper/main.py
+++ python/webscraper/jobscraper/main.py
@@ -1,4 +1,27 @@
 from bs4 import BeautifulSoup
 import requests
+import webbrowser
 
-rq = requests.get('https://www.seek.com.au/python-jobs')
+rq = requests.get('https://www.seek.com.au/python-jobs/in-All-Sydney-NSW').text
+soup = BeautifulSoup(rq, 'lxml')
+
+job_div = soup.find_all('h3', class_='_1wkzzau0 a1msqi4y lnocuo0 lnocuol _1d0g9qk4 lnocuos lnocuo21')
+employers_span = soup.find_all('span', class_='_1wkzzau0 a1msqi4y lnocuo0 lnocuo2 lnocuo21 _1d0g9qk4 lnocuod')
+
+# prints job titles
+current_roles = []
+for i in job_div:
+    current_roles.append(i.text)
+
+employers = []
+for i in range(0, len(employers_span)):
+    employers.append(employers_span[i].text)
+
+for i in range(len(current_roles)):
+    print(f'{current_roles[i]} {employers[i]}\n')
+
+if(input("open?\n").lower() == 'yes'):
+    webbrowser.open_new('https://www.seek.com.au/python-jobs/in-All-Sydney-NSW')
+
+
+
