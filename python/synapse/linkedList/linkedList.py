b2c0de72a0f4475164b7db5f6ad9e5cc912d705d
Leonard Zhong <leonard.c.zhong@gmail.com>
2023-04-24 19:49:19 +1000

    nodes

--- python/synapse/linkedList/linkedList.py
+++ python/synapse/linkedList/linkedList.py
@@ -1,13 +1,3 @@
 class linkedList:
     def __init__(self):
-        self.value
-        self.next
-
-    def getValue(self):
-        return self.value
-    
-    def getNext(self):
-        return self.next
-    
-    def printValue(self):
-        print(self.value)
+        self.head = None
