383a6c8d022b7c0f3bb84a3b03263c4d20df3b3f
Leonard Zhong <leonard.c.zhong@gmail.com>
2023-04-24 19:36:45 +1000

    linked lists
    creating a linked list in py

---
+++ python/synapse/linkedList/linkedList.py
@@ -0,0 +1,13 @@
+class linkedList:
+    def __init__(self):
+        self.value
+        self.next
+
+    def getValue(self):
+        return self.value
+    
+    def getNext(self):
+        return self.next
+    
+    def printValue(self):
+        print(self.value)
---
+++ python/synapse/linkedList/main.py
@@ -0,0 +1,8 @@
+from linkedList import *
+
+def main():
+    return 0
+
+if __name__ == '__main__':
+    main()
+
