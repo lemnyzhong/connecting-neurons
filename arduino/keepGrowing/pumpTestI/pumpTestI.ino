130dcaa1193438d90570210cb45042f5da3da1bd
Leonard Zhong <leonard.c.zhong@gmail.com>
2023-05-09 23:59:52 +1000

    pumping
    pumps work
    to do:
        work out time schedule for the different cycles

--- arduino/keepGrowing/pumpTestI/pumpTestI.ino
+++ arduino/keepGrowing/pumpTestI/pumpTestI.ino
@@ -1,5 +1,6 @@
-int potentiometer = A0;
-int pump = 0;
+// int potentiometer = A0;
+int pump = 13;
+int reset = 4;
 int val = 0;
 
 void turnOnPump() {
@@ -10,29 +11,31 @@ void turnOffPump() {
   digitalWrite(pump, LOW);
 }
 
-int readVal() {
-  val = analogRead(potentiometer);
-  return val;
-}
+// int readVal() {
+//   val = analogRead(potentiometer);
+//   return val;
+// }
 
 void setup() {
   Serial.begin(9600);
 
   pinMode(pump, OUTPUT);
 
-  pinMode(potentiometer, INPUT);
-
+  digitalWrite(4, HIGH);
+  pinMode(4, OUTPUT);
 }
 
 void loop() {
-  // analogWrite(pump, 155);
-  // delay(5000);
-  // turnOffPump();
-  // delay(5000);
-  // turnOnPump();
-  // delay(5000);
-  // turnOffPump();
-  // delay(30000);
+  unsigned long time = millis();
+  Serial.println(time);
+
+  if (time > 5000) {
+    turnOnPump();
+  }
+
+  if (time > 10000) {
+    digitalWrite(4, LOW);
+  }
 
 }
 
