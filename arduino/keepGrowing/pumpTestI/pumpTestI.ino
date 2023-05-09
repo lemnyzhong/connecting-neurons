eb4beb402a2b7f9a4239aebf9b1d815e03134881
Leonard Zhong <leonard.c.zhong@gmail.com>
2023-05-09 21:08:28 +1000

    on off
    pump test

--- arduino/keepGrowing/pumpTestI/pumpTestI.ino
+++ arduino/keepGrowing/pumpTestI/pumpTestI.ino
@@ -1,9 +1,40 @@
+int potentiometer = A0;
+int pump = 0;
+int val = 0;
+
+void turnOnPump() {
+  digitalWrite(pump, HIGH);
+}
+
+void turnOffPump() {
+  digitalWrite(pump, LOW);
+}
+
+int readVal() {
+  val = analogRead(potentiometer);
+  return val;
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(pump, OUTPUT);
+
+  pinMode(potentiometer, INPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // analogWrite(pump, 155);
+  // delay(5000);
+  // turnOffPump();
+  // delay(5000);
+  // turnOnPump();
+  // delay(5000);
+  // turnOffPump();
+  // delay(30000);
 
 }
+
+
+
